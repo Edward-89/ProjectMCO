f2ad22ae0723525cf19813de3e405d1c901ec8c1
Edward-89 <voronkov_eduard@mail.ru>
2015-07-06 03:41:13 +0300

    remove excess code

--- src/com/diplom/project/panels/PersentPanel.java
+++ src/com/diplom/project/panels/PersentPanel.java
@@ -23,7 +23,6 @@ public class PersentPanel extends JPanel {
 		
 		this.value = new double[table.getColumnCount()-1];
 		
-		//разбиение панели на сетку
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths  = new int[]{0, 0, 0, 0, 0,
 												0, 0, 0, 0, 0,
@@ -48,11 +47,10 @@ public class PersentPanel extends JPanel {
 
 	}
 	
-	// *** метод размещающий компоненты на панеле ***
 	private void createComponentsOfPanel(JTable table){
 		
 		MyTableModel model = (MyTableModel) table.getModel();
-		String[] str = model.getColumn(); // получение 
+		String[] str = model.getColumn(); // ГЇГ®Г«ГіГ·ГҐГ­ГЁГҐ 
 		int len = str.length-1;
 		final JLabel label = new JLabel("Persent method %");
 		addNewComponent(label, 2, 0);
@@ -63,19 +61,9 @@ public class PersentPanel extends JPanel {
 			addNewComponent(textField, 2, row + 1);
 			addKeyFilter(textField, index);
 			textFilds.add(textField);
-			textField.addActionListener(new ActionListener() {
-			
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-//						setValue(index, Double.valueOf(textField.getText()));
-						System.out.println(Arrays.toString(value));
-				}
-			});
 		}
 		
 	}
-	 //****** фильтр ввода чисел и точки ********
 	 private String digitsOnlyFilter (String value) {
 	       int len = value.length();
 	       String result = "";
@@ -94,15 +82,12 @@ public class PersentPanel extends JPanel {
 	       return result;
 	    }
 	 
-	 // ***** обработчик нажатия кнопок при вводе в текстовое поле ******
 	 private void addKeyFilter(final JTextField field, final int index) {
 	        field.addKeyListener(new KeyListener() {
 	            public void keyTyped(KeyEvent e) {}
 	            public void keyPressed(KeyEvent e) {}
 	            public void keyReleased(KeyEvent e) {
 	                field.setText(digitsOnlyFilter(field.getText()));
-	                //блок проверки, исключающий ошибку при пустом поле ввода. 
-	                // По умолчанию пустое поле равно 0
 	                try{
 	                	setValue(index, Double.valueOf(field.getText()));
 	                	if(Double.valueOf(field.getText()) > 1.0){
@@ -113,9 +98,6 @@ public class PersentPanel extends JPanel {
 	                } catch(NumberFormatException exc){
 	                	setValue(index, 0d);
 	                }
-	                
-	                
-					System.out.println(Arrays.toString(value));
 	            }
 	        });
 	    }
@@ -129,7 +111,6 @@ public class PersentPanel extends JPanel {
 	 }
 	 
 	
-	 // *** добавление нового компонента на панель в заданную ячейку сетки разбиения ****
 	 private void addNewComponent(JComponent component, int x, int y){
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(0, 0, 5, 5);
@@ -138,19 +119,16 @@ public class PersentPanel extends JPanel {
 		add(component, gbc);
 	}
 	
-	 // *** установка значения элемента массива value *****
 	 private void setValue(int index, double value){
 	    	this.value[index] = value;
 	 }
 	
 	
-	 // *** установка активности/неактивности  полей ввода ***
 	 public void setEnabledTextFields(boolean bool){
 		for(JTextField jtf : textFilds)
 			jtf.setEnabled(bool);
 	 }
 	
-	 //*** преобразование значений введенных данных в приоритеты ***
 	 private int[] persentToPriority(double[] value){
 		double[] sortValue = Arrays.copyOf(value, value.length);
 		Arrays.sort(sortValue);
@@ -167,7 +145,6 @@ public class PersentPanel extends JPanel {
 		return priority;
 	 }
 	
-	 // *** открытый доступ к получению массива priority ***
 	 public int[] getPriority(){
 		return persentToPriority(value);
 	 }
